cde11aefa716f00201b12236fffb48f3dc06aa08
AlcheMan0t <147844098+AlcheMan0t@users.noreply.github.com>
2023-10-13 18:10:35 +0200

    Update myNumber.java

--- L3/myNumber.java
+++ L3/myNumber.java
@@ -9,10 +9,27 @@ public class myNumber {
 	}
 	int getnum() {
 		return num;
+		
 	}
+	int getnum2() {
+		return num2;
+	}
+	
 	void setNum(int i) {
 		num=i;
 		num2=10*i;}
+	void description() {
+		System.out.println("The value of number 1 is: "+ num);
+		System.out.println("The value of number 2 is: "+num2);
+		
+	}
+	 float average () {
+		return (num+num2)/2;
+	}
+	public myNumber (int num, int num2) {
+		this.num= num;
+		this.num2=num2;
+	}
 
 	public static void main (String[] args ) {
 		myNumber A=new myNumber ();
@@ -20,6 +37,7 @@ public class myNumber {
 		A.setNum(10);
 		System.out.println(A.getnum());
 		System.out.println(A.num2);
+		System.out.println(A.average());
 	}
 
 }
