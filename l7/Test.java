30a3d995ede801c0d5876cd9ad2c69a39aa212b8
AlcheMan0t <147844098+AlcheMan0t@users.noreply.github.com>
2023-11-24 18:11:09 +0100

    Add files via upload

---
+++ l7/Test.java
@@ -0,0 +1,17 @@
+package ass1;
+
+public class Test {
+String s;
+public Test (){
+	s="Programming languages";
+}
+public void method1() {
+	System.out.println(s);
+}
+public void method2 (int n) {
+	System.out.println(n);
+}
+private void method3 () {
+	System.out.println("Private method is invoked");
+}
+}
