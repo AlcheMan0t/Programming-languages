6c728fa284331abe342f630eb4d8ebea4201643b
AlcheMan0t <147844098+AlcheMan0t@users.noreply.github.com>
2023-12-06 19:45:06 +0100

    Create assignment1.java

---
+++ h2/assignment1.java
@@ -0,0 +1,15 @@
+package finalexam;
+
+public class Assignment1 {
+
+}
+package finalexam;
+//Aleksandar Alche 5451
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
